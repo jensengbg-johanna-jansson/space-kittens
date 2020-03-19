2c81ba9c56f4df6f7d38b1a157de607639c1df97
Johanna Jansson <joja92@hotmail.com>
2020-03-19 15:43:15 +0100

    Created MenuItem and MenuButton

--- src/components/MenuButton.vue
+++ src/components/MenuButton.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="buttonContainer">
+        <img src="../assets/graphics/add.svg" alt="">
     </div>
 </template>
 
@@ -10,5 +11,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    .buttonContainer {
+        border-radius: 50%;
+        background: $brown;
+        width: 32px;
+        height: 32px;
+        padding: 0.5rem;
+    }
 </style>
