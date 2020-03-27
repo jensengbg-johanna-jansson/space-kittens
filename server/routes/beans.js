4a61aa316b899b86f24de70390b88969fdc4f0b0
Johanna Jansson <joja92@hotmail.com>
2020-03-27 12:02:41 +0100

    Created database and backend

--- server/routes/beans.js
+++ server/routes/beans.js
@@ -4,6 +4,16 @@ const fs = require('fs');
 const uuid = require('uuid-random');
 const { generateOrderNr, generateETA } = require('../utils/utils');
 
+//setup database
+const lowdb = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('data/database.json');
+const database = lowdb(adapter);
+const bodyParser = require('body-parser');
+
+// Create application/json parser
+var jsonParser = bodyParser.json();
+
 router.get('/', async (req, res) => {
     const menu = fs.createReadStream('data/menu.json');
     menu.pipe(res);
@@ -25,6 +35,42 @@ router.get('/key', (req, res) => {
         key: uuid()
     }
     res.send(JSON.stringify(key));
-})
+});
 
-module.exports = router
+router.get('/user/:uuid', async (req, res) => {
+    const user = await database.get('users')
+    .filter({uuid: parseInt(req.params.uuid)})
+    .value();
+    
+    res.send(user);
+});
+
+router.post('/user', jsonParser, async (req, res) => {
+    await database.get('users')
+    .push(req.body)
+    .write();  
+    
+    res.send(JSON.stringify(req.body));
+});
+
+router.get('/order/:uuid', async (req, res) => {
+    const orders = await database.get('orders')
+                                .filter({uuid: parseInt(req.params.uuid)})
+                                .value();
+    
+    res.send(orders);
+});
+
+router.post('/order', jsonParser, async (req, res) => {
+    const addOrder = await database.get('orders')
+    .push(req.body)
+    .write();
+
+    if(addOrder) {
+        res.send(req.body);
+    } else {
+        res.send('Database Error: Could not add order');
+    }
+});
+
+module.exports = router;
